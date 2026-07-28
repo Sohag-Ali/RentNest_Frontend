6cd553e67365bff56705348571486246ef55e1f0
sohag142 <sohag2879@gmail.com>
2026-07-28 21:11:22 +0600

    Folder structure create

---
+++ app/(dashboardRoute)/dashboard/landlord/requests/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            landlord requests
+        </div>
+    );
+};
+
+export default page;
---
+++ app/(publicRoute)/properties/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            property details page 
+        </div>
+    );
+};
+
+export default page;
